fabd935c94a9d420416c2a6b598798528ec4bdcf
GJyeon <junyeon03@gmail.com>
2026-03-25 16:40:48 +0900

    실습 예제

---
+++ 03_VUE/01_basic/07_컴포넌트심화/slot-test/src/App2.vue
@@ -0,0 +1,35 @@
+<script setup>
+import NoSlotTest from './components/example1/NoSlotTest.vue';
+import SlotTest from './components/example1/SlotTest.vue';
+import BaseCard from './components/example2/BaseCard.vue';
+
+NoSlotTest;
+</script>
+
+<template>
+  <div>
+    <BaseCard>
+      <template #header>
+        공지사항
+      </template>
+
+      <!-- #default : template 없이 작성해도 기본 슬롯으로 들어감 -->
+       <p>3월 25일 이전에 풀던 example 컴포넌트로 분리해서 만드는거 완성하세요</p>
+       <p><strong>이름: </strong>홍길동</p>
+      <p><strong>직무: </strong>프론트개발자</p>
+      <p><strong>경력: </strong>3년</p>
+      <template #footer>
+        <button>취소</button>
+        <button>저장</button>
+      </template>
+    </BaseCard>
+  </div>>
+</template>
+
+<style scoped>
+
+.card-ros{
+  display: flex;
+  gap: 20px;
+}
+</style>
---
+++ 03_VUE/01_basic/07_컴포넌트심화/style-test/src/components/ScopedCssDemo.vue
@@ -0,0 +1,21 @@
+<template>
+    <h2>1. 전역 CSS</h2>
+    <div class="box">Scoped CSS가 적용된 박스입니다.</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+/* 
+Scoped속성
+- 이 컴포넌트에만 슨타일을 적용
+- Vue가 data-v-해시값속성을 생성하여 적용함
+- 다른컴포넌트의 같은 클래스명과 충돌하지 ㅇ낳음
+*/
+.box {
+    background-color: aqua;
+    border: 2px solid blue;
+}
+</style>
